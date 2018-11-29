ba1564747fa3339e7522ab7eef42d3f5716d1210
sam11385 <samluedke@gmail.com>
2018-11-29 18:59:19 -0500

    Set up basic routing.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+const things = require('./things.js');
+
 app.get('/', function(req, res) {
   res.send('Hello world!');
 });
+
+app.use('/things', things);
 
 app.listen(3000);
