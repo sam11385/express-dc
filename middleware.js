1a41eb2f212b736537adeabb9fc2c5ee25e64fcf
sam11385 <samluedke@gmail.com>
2018-11-29 21:19:26 -0500

    Add middleware file.

---
+++ middleware.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+//Simple request time logger
+app.use(function(req, res, next) {
+  console.log('A new request received at ' + Date.now());
+
+  // This next() function tells the server that more processing is
+  // required for the current request and is in the next middleware
+  // function/route handler.
+  next();
+});
+
+app.listen(3000);
