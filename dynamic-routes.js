65f4713adffc1802dc72bfaded30598904c465e2
sam11385 <samluedke@gmail.com>
2018-12-07 15:55:18 -0500

    Set up dynamic routes.

--- dynamic-routes.js
+++ dynamic-routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/:id', function(req, res) {
-  res.send('The route id you specified is ' + req.params.id);
+  res.send('The route id is ' + req.params.id);
 });
+
 app.listen(3000);
