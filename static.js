fa3d0e6d0b713a5eced31f345048fda7ee7372f1
sam11385 <samluedke@gmail.com>
2018-12-11 13:36:09 -0500

    FIles for express static class.

--- static.js
+++ static.js
@@ -8,4 +8,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //setting middleware
 app.use(express.static(__dirname + '/public')); //Serves resources from public folder
 
+app.get('/about', function(req, res) {
+  res.send('About Page');
+});
+
 app.listen(3000);
