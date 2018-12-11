c85fad2da744b531e51167f3ffb5e752428bd3fa
sam11385 <samluedke@gmail.com>
2018-12-10 21:27:40 -0500

    Run static server.

--- static.js
+++ static.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //setting middleware
-app.use(express.static(__dirname + 'public')); //Serves resources from public folder
+app.use(express.static(__dirname + '/public')); //Serves resources from public folder
 
-const server = app.listen(3000);
+app.listen(3000);
